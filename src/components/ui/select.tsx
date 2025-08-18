69fa2a3471befec4f7f6c2ab57b915c8e96798da
Nikita Bragin <branikita@gmail.com>
2025-08-18 17:25:10 +0300

    refactor: remove opacity from ChevronDownIcon in SelectTrigger for improved visibility

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -25,7 +25,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDownIcon className="h-4 w-4 opacity-50" />
+      <ChevronDownIcon className="h-4 w-4" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
